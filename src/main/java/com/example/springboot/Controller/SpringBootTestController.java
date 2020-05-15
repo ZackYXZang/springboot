57746bea4ae85b26f613f4ce8c65e97e84153b46
ZackYXZang <zangyuxiang3038@163.com>
2020-05-15 18:54:50 +0800

    First Spring Boot Project Upload To Locol Repository.

---
+++ src/main/java/com/example/springboot/Controller/SpringBootTestController.java
@@ -0,0 +1,23 @@
+package com.example.springboot.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("test")
+public class SpringBootTestController {
+
+    @Value("${test.name}")
+    private String name;
+
+    @Value("${test.remark}")
+    private String remark;
+
+    @RequestMapping("remark")
+    public String returnRemark(){
+        return name +"'s " + remark + "!";
+    }
+}
