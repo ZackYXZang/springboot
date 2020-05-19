75b6c8a3b9cc234b2a1d691ed0ee51b5243e21de
yuxiangzang <zangyuxiang3038@163.com>
2020-05-19 15:29:21 +0800

    spring boot 配置异步任务

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -10,6 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(basePackages= {"com.example"})
 //开启定时任务
 @EnableScheduling
+//开启异步调用方法
+@EnableAsync
 public class SpringbootApplication {
 
     public static void main(String[] args) {
