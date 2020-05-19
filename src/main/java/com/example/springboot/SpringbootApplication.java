9de8b7a949276d241d9f43a5e7210faeef05856a
yuxiangzang <zangyuxiang3038@163.com>
2020-05-19 12:04:46 +0800

    定时任务

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,8 +2,14 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
+@ComponentScan(basePackages= {"com.example"})
+//开启定时任务
+@EnableScheduling
 public class SpringbootApplication {
 
     public static void main(String[] args) {
