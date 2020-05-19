1215df93204cdf58768fd85d08f222b4c01072db
yuxiangzang <zangyuxiang3038@163.com>
2020-05-19 15:53:59 +0800

    spring boot 配置拦截器2

--- src/main/java/com/example/springboot/config/WebMvcConfigurer.java
+++ src/main/java/com/example/springboot/config/WebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.example.springboot.config;
 
 import com.example.springboot.Controller.interceptor.OneInterceptor;
+import com.example.springboot.Controller.interceptor.TwoInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,6 +13,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
          *  * 拦截器按照顺序执行
          */
         registry.addInterceptor(new OneInterceptor()).addPathPatterns("/one/**");
+        registry.addInterceptor(new TwoInterceptor()).addPathPatterns("/two/**").addPathPatterns("/one/**");
         super.addInterceptors(registry);
     }
 }
