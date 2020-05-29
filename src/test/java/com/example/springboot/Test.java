93ce437f7f787ad7387005e6c745cacc49770cf2
yuxiangzang <zangyuxiang3038@163.com>
2020-05-29 14:47:19 +0800

    practice， no meaning

--- src/test/java/com/example/springboot/Test.java
+++ src/test/java/com/example/springboot/Test.java
@@ -1,6 +1,7 @@
 package com.example.springboot;
 
 
+import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thymeleaf.util.DateUtils;
@@ -8,17 +9,28 @@ import org.thymeleaf.util.DateUtils;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Test {
 
     private static Logger log = LoggerFactory.getLogger(Test.class);
 
     public static void main(String[] args) {
-        String test = String.format("%s_%s_a_his", "12", "280315565");
-        System.out.println(test);
-        List<String> months = getLastMonths(2);
-        log.info("months{}", months);
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("username", "zangyx");
+        map.put("token", "adfasdf");
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("data", map);
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("code", "200");
+        map2.put("data", map1);
+        JSONObject js = new JSONObject(map2);
+        System.out.println(JSONObject.toJSONString(map2));
+
+
 
 
 
