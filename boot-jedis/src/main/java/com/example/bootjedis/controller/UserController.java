351c72041e4c9c17e80e749817159e8900536f4c
yuxiangzang <zangyuxiang3038@163.com>
2020-06-30 16:45:35 +0800

    jedis 学习，未完

---
+++ boot-jedis/src/main/java/com/example/bootjedis/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.bootjedis.controller;
+
+/**
+ * @author yuxiangzang
+ * @create 2020-05-29-6:39 下午
+ * @desc
+ **/
+public class UserController {
+}
---
+++ boot-jedis/src/main/java/com/example/bootjedis/pojo/User.java
@@ -0,0 +1,17 @@
+package com.example.bootjedis.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @author yuxiangzang
+ * @create 2020-05-29-6:40 下午
+ * @desc 用户实体
+ **/
+public class User implements Serializable {
+
+    private String id;
+
+    private String name;
+
+    private Integer age;
+}
