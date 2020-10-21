4f3e4463a3981ca2dd31f2f9fec0f84e1415f87d
yuxiangzang <zangyuxiang3038@163.com>
2020-10-21 15:20:42 +0800

    111

--- boot-lettuce/src/main/java/com/example/bootlettuce/entity/test.java
+++ boot-lettuce/src/main/java/com/example/bootlettuce/entity/test.java
@@ -1,10 +1,19 @@
 package com.example.bootlettuce.entity;
 
+import java.util.List;
+import lombok.Data;
+
 /**
  * @author yuxiangzang
  * @create 2020-08-10-3:36 下午
  * @desc  只是为了测试检出新分支
  **/
-public class test {
+@Data
+public class test{
 
+  private String id1;
+  private String name1;
+  private List<String> ab;
+
+  private Boolean flag;
 }
