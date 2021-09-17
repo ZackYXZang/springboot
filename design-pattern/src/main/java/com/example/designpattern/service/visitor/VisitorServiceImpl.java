ec7a0652593e4aa4dcab5b678bd0d3be48f51b97
yuxiangzang <zangyuxiang3038@163.com>
2021-09-17 23:54:52 +0800

    todo

---
+++ design-pattern/src/main/java/com/example/designpattern/service/visitor/VisitorServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.designpattern.service.visitor;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 访问者模式
+ * 在结构不变的情况下动态改变对于内部元素的动作
+ * todo：还没有听课，待完善
+ */
+@Service
+public class VisitorServiceImpl {
+
+}
